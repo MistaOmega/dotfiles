8e8c822fadcaad44d65beca481b945b5aac5ece0
mistaomega <jack.nash6@gmail.com>
2024-04-26 22:41:56 +0100

    Added markdown preview

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -4,4 +4,12 @@ return {
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     ft = "hypr",
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 }
